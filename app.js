57a3b2bbb803d4324a62d08275d4e96a9b8d3847
Santiago Orozco <deyvidabch1103@gmail.com>
2026-04-28 19:42:42 -0500

    Promedio de tres notas #3

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-let nombre = "Pepito"
-const PI = 3.1416
-console.log("Tu nombre es: ", nombre, "Pi: ", PI)
-
+/* 3. Promedio de tres notas */
+alert("Ahora el promedio de tres notas ");
+let Nota1 = parseFloat(prompt("Ingrese la nota 1: "));
+let Nota2 = parseFloat(prompt("Ingrese la nota 2: "));
+let Nota3 = parseFloat(prompt("Ingrese la nota 3: "));
+let promedio = (Nota1 + Nota2 + Nota3) / 3;
+alert("El promedio de las notas es: " + promedio);
