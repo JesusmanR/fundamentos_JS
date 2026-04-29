f5429eaee6b3e169cc6218a39b0866b70eeb3e95
Santiago Orozco <orosantt@gmail.com>
2026-04-28 19:08:27 -0500

    Saludo personalizado termiando #1

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let nombre = "Pepito"
-const PI = 3.1416
-console.log("Tu nombre es: ", nombre, "Pi: ", PI)
+/* C. Actividades de apropiación del conocimiento. */
 
+/* Ejercicio 1. Saludo personalizado */
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuántos años tienes?");
+alert("Hola " + nombre + ", tu edad es " + edad);
