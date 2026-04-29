40905ce3a3b3065516d29fdb7c42926918a0e0e4
Santiago Orozco <deyvidabch1103@gmail.com>
2026-04-28 20:04:28 -0500

    Se creo Ejercicio1.js y se termino el primer punto #1

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+/* C. Actividades de apropiación del conocimiento. */
+
+/* Ejercicio 1. Saludo personalizado */
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuántos años tienes?");
+alert("Hola " + nombre + ", tu edad es " + edad);
