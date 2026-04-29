2f7a4461aeb86e5e6a66adfcbd81c87313045bc7
Santiago Orozco <deyvidabch1103@gmail.com>
2026-04-28 19:41:26 -0500

    Conversion de monedas terminado_ #4

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-/* 3. Promedio de tres notas */
-alert("Ahora el promedio de tres notas ");
-let Nota1 = parseFloat(prompt("Ingrese la nota 1: "));
-let Nota2 = parseFloat(prompt("Ingrese la nota 2: "));
-let Nota3 = parseFloat(prompt("Ingrese la nota 3: "));
-let promedio = (Nota1 + Nota2 + Nota3) / 3;
-alert("El promedio de las notas es: " + promedio);
+/* 4. Conversión de monedas */
+alert("Ahora convertiremos de dolares a cop");
+let dol = parseFloat(prompt("Ingrese el valor en dolares: "));
+let valor = dol * 3564.5;
+alert("El valor de tus dolares a cop es: " + valor);
