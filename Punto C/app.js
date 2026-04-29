cd8d07d08bc22142889b4c909f01b4b2e7e95090
MaximontiX <deyvidabch1103@gmail.com>
2026-04-28 23:07:01 -0500

    Se desarrollo el punto 2

--- Punto C/app.js
+++ Punto C/app.js
@@ -8,3 +8,11 @@ console.log("Tu nombre es: ", nombre, "Pi: ", PI)
 let nombre = prompt("¿Cuál es tu nombre?");
 let edad = prompt("¿Cuántos años tienes?");
 alert("Hola " + nombre + ", tu edad es " + edad);
+
+
+/* Ejercicio 2 Área de un rectángulo */
+alert ("Ahora calcularemos el area de un triangulo");
+let base = parseFloat(prompt("Ingrese la base del triangulo: "));
+let altura = parseFloat(prompt("Ingrese la altura: "));
+let Area = (base * altura) / 2 ;
+alert ("El area del triangulo es: " + Area);
