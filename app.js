5014bd6099e0cb5401424725919a8e020a49c877
Santiago Orozco <orosantt@gmail.com>
2026-04-28 19:17:41 -0500

    Area de rectangulo terminado #2

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-let nombre = "Pepito"
-const PI = 3.1416
-console.log("Tu nombre es: ", nombre, "Pi: ", PI)
+/* 2. Área de un rectángulo */
+alert("Ahora calcularemos el area de un triangulo");
+let base = parseFloat(prompt("Ingrese la base del triangulo: "));
+let altura = parseFloat(prompt("Ingrese la altura: "));
+let Area = (base * altura) / 2;
+alert("El area del triangulo es: " + Area);
 
