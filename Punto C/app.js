5d7200811b75b2888e7a404b4914dd7b56b516fb
MaximontiX <deyvidabch1103@gmail.com>
2026-04-28 23:31:40 -0500

    Se desarrollo el punto 4

--- Punto C/app.js
+++ Punto C/app.js
@@ -25,3 +25,9 @@ let Nota2 = parseFloat(prompt("Ingrese la nota 2: "));
 let Nota3 = parseFloat(prompt("Ingrese la nota 3: "));
 let promedio = ( Nota1 + Nota2 + Nota3)/3 ;
 alert ("El promedio de las notas es: " +promedio);
+
+/* Ejercicio 4  Conversión de monedas*/
+alert ("Ahora convertiremos de dolares a cop");
+let dol = parseFloat(prompt("Ingrese el valor en dolares: "));
+let valor = ( dol * 3564.5);
+alert ("El valor de tus dolares a cop es: "+ valor);
