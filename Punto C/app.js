4296571bded286e11f0e91addc761231772346d3
MaximontiX <deyvidabch1103@gmail.com>
2026-04-28 23:16:33 -0500

    Se desarrollo el punto 3

--- Punto C/app.js
+++ Punto C/app.js
@@ -16,3 +16,12 @@ let base = parseFloat(prompt("Ingrese la base del triangulo: "));
 let altura = parseFloat(prompt("Ingrese la altura: "));
 let Area = (base * altura) / 2 ;
 alert ("El area del triangulo es: " + Area);
+
+
+/* Ejercicio 3 Promedio de tres notas */
+alert ("Ahora el promedio de tres notas ");
+let Nota1 = parseFloat(prompt("Ingrese la nota 1: "));
+let Nota2 = parseFloat(prompt("Ingrese la nota 2: "));
+let Nota3 = parseFloat(prompt("Ingrese la nota 3: "));
+let promedio = ( Nota1 + Nota2 + Nota3)/3 ;
+alert ("El promedio de las notas es: " +promedio);
